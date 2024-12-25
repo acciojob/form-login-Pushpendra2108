71c12d18c65966cf57c213af163c29f4183aafae
Pushpendra kumar sahu <153269194+Pushpendra2108@users.noreply.github.com>
2024-12-25 14:58:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	let firstName = document.form[form1][fname].value;
+	let lastName = document.form[form1][lname].value;
+	
+  alert( firstName + " " + lastName); 
 }
